3d62e0d369f9dd287d90f0be628e8e45e43c74af
manthannayaks <bomanthan51@gmail.com>
2025-09-02 13:18:50 +0530

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { InputField } from './InputField'
+export { DataTable } from './DataTable'
+export type { InputFieldProps } from './InputField'
+export type { DataTableProps, Column } from './DataTable'
